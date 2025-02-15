bcb6e07cb7d2fd0955db159979c39b3d1395f2e9
Anton Schukin <antonschukin@gmail.com>
2025-02-15 21:13:31 +0200

    Local storage save + reset button

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,7 +1,23 @@
 <script setup>
 import { ref } from "vue";
 
-const count = ref(0);
+const initialCount = localStorage.getItem("count") || 0;
+const count = ref(initialCount);
+
+function increment() {
+  count.value++;
+  localStorage.setItem("count", count.value);
+}
+
+function decrement() {
+  count.value--;
+  localStorage.setItem("count", count.value);
+}
+
+function reset() {
+  count.value = 0;
+  localStorage.removeItem("count");
+}
 </script>
 
 <template>
@@ -9,14 +25,18 @@ const count = ref(0);
     <div class="button-wrapper">
       <button
         class="button"
-        @click="count++"
+        @click="increment()"
       >Increment</button>
       <button
         class="button"
-        @click="count--"
+        @click="decrement()"
       >Decrement</button>
     </div>
     <p class="counter-text">The current count is: {{ count }}.</p>
+    <button
+      class="reset-button"
+      @click="reset()"
+    >Reset</button>
   </div>
 </template>
 
@@ -35,9 +55,9 @@ const count = ref(0);
   height: 50px;
   width: 120px;
   border-radius: 8px;
-  border: 1px solid #c794ff;
+  border: 1px solid #2e96a1;
   cursor: pointer;
-  background-color: #d6b1ff;
+  background-color: #5fc6d2;
   transition: background-color 0.2s ease-in-out;
   font-size: 14px;
   font-family: "Open Sans";
@@ -45,12 +65,27 @@ const count = ref(0);
   font-weight: 700;
 }
 .button:hover {
-  background-color: #c794ff;
+  background-color: #2e96a1;
 }
 .counter-text {
   margin-top: 20px;
   font-size: 20px;
   font-family: "Open Sans";
   color: #202020;
+}
+.reset-button {
+  margin-top: 20px;
+  border-radius: 8px;
+  height: 28px;
+  width: 60px;
+  background-color: #a20200;
+  color: white;
+  border: 1px solid black;
+  transition: background-color 0.2s ease-in-out;
+  cursor: pointer;
+  font-family: "Open Sans";
+}
+.reset-button:hover {
+  background-color: #740200;
 }
 </style>
