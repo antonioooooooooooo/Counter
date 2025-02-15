96ddb7d01b3491ed94b7c2adc96930aaabb257de
Anton Schukin <antonschukin@gmail.com>
2025-02-15 18:27:40 +0200

    base url

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Counter/',
   plugins: [
     vue(),
     vueDevTools(),
